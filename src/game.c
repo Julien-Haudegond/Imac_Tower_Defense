334f1a0f03b6611db43b422151e85c1524a53ef3
Mathilde <mathildedumoulin54@gmail.com>
2019-06-04 16:57:16 +0200

    Plus de gestion des arguments

--- src/game.c
+++ src/game.c
@@ -177,7 +177,9 @@ int playGame(SDL_Surface* surface, const char* itdPath)
 
     /* MAIN LOOP */
     int loop = 1;
-    while(loop) 
+
+
+    while(loop)
     {
         /* Time at the beginning of the loop */
         Uint32 startTime = SDL_GetTicks();
@@ -498,6 +500,7 @@ int playGame(SDL_Surface* surface, const char* itdPath)
             }
         }
 
+    
         /* Passed time */
         Uint32 elapsedTime = SDL_GetTicks() - startTime;
         /* If too few time, the program is paused */
--- src/main.c
+++ src/main.c
@@ -9,16 +9,6 @@
 
 int main(int argc, char const *argv[])
 {
-
-	 //Checking arguments
-    char itdPath[70]="data/";
-    if(argc >= 2 && strcmp(checkITDFile(argv[1]), "itd") == 0){        
-        strcat(itdPath,argv[1]);
-    }else{
-        fprintf(stderr, "Second argument must be an itd file.");
-        exit(EXIT_FAILURE);
-    }
-
     playIntro();
 
 	return EXIT_SUCCESS;
